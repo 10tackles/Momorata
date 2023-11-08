005c6a6fd88cb2555ee060f63639a398c50856ef
Olu <sadegoroye@gmail.com>
2023-11-08 03:26:31 -0600

    Adding Entity Framework

---
+++ Momorata/Momorata.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Momorata.Core.Models;
+using System.Data.Entity;
+
+namespace Momorata.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
